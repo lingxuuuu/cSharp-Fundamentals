d2ba49b99e641862c05430a70c8477f928be65ce
Ling Xu <36904516+xulingggg@users.noreply.github.com>
2021-02-09 08:53:51 -0800

    commit on master

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 255; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i=1; i< 101; i++) 
+            {
+                if (i%3 == 0 && i%5==0)
+                {
+                    continue;
+                }
+                  else if (i%3 == 0 || i%5==0)
+                {
+                    Console.Write(i);
+                }
+            }
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i + " FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine(i + " Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(i + " Buzz");
+                }
+            }
+    }
+}   
+}
